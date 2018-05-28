41f7a58c2d068af13000d1834690687bcd813784
Jens (Aorus) <mikson60@gmail.com>
2018-05-28 13:06:49 +0300

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -19,18 +19,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'jens',
-        text: 'Yo, wassup',
-        createdAt: 123
-    });
-
     socket.on('disconnect', (reason) => {
         console.log('Client disconnected');
     });
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 });
 
