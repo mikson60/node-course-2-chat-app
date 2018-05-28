3f5271ba87ffe41e3fc3f73f66ea1bc5b9781b3c
Jens (Aorus) <mikson60@gmail.com>
2018-05-28 11:00:35 +0300

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -19,8 +19,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'jens',
+        text: 'Yo, wassup',
+        createdAt: 123
+    });
+
     socket.on('disconnect', (reason) => {
         console.log('Client disconnected');
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
     });
 });
 
